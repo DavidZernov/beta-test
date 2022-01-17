31b3c1e23382f86b49fc55cec9db001c1df35e6a
DavidZernov <95302459+DavidZernov@users.noreply.github.com>
2022-01-17 13:37:48 +0300

    Add files via upload

---
+++ settings/jsSettingsCategory.js
@@ -0,0 +1,15 @@
+//     Управление видимости категорий товаров true\false
+let clothes = true
+let cosmetics = true
+let products = true
+let house = true
+let auto = true
+let presents = true
+let sales = true
+let sport = true
+let entertainment = true
+let toys = true
+let accessories = true
+let pets = true
+let mothers = true
+//     Управление видимости категорий товаров true\false
---
+++ settings/jsSettingsClasses.js
@@ -0,0 +1,72 @@
+//     Управление видимости (категорий) ---> КЛАССОВ товаров true\false
+let clothesMan = true
+let clothesWoman = true
+let clothesUnisex = true
+
+let cosmeticsMan = false
+let cosmeticsWoman = false
+let cosmetics3 = false
+
+let products1 = false
+let products2 = false
+let products3 = false
+
+let house1 = false
+let house2 = false
+let house3 = false
+let house4 = false
+let house5 = false
+
+let auto1 = false
+let auto2 = false
+let auto3 = false
+let auto4 = false
+let auto5 = false
+
+let presents1 = false
+let presents2 = false
+let presents3 = false
+let presents4 = false
+let presents5 = false
+
+let sales1 = false
+let sales2 = false
+let sales3 = false
+let sales4 = false
+let sales5 = false
+
+let sport1 = false
+let sport2 = false
+let sport3 = false
+let sport4 = false
+let sport5 = false
+
+let entertainment1 = false
+let entertainment2 = false
+let entertainment3 = false
+let entertainment4 = false
+let entertainment5 = false
+
+let toys1 = false
+let toys2 = false
+let toys3 = false
+let toys4 = false
+let toys5 = false
+
+let accessories1 = false
+let accessories2 = false
+let accessories3 = false
+let accessories4 = false
+let accessories5 = false
+
+let pets1 = false
+let pets2 = false
+let pets3 = false
+let pets4 = false
+let pets5 = false
+
+let mothers1 = false
+let mothers2 = false
+let mothers3 = false
+let mothers4 = false
+let mothers5 = false
